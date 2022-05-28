adf1a2d2bddce1ce2a2957d5d93f2aff1d56d274
Grayson-Orr <grayson.orr@op.ac.nz>
2022-05-29 10:50:47 +1200

    refactor: update env vars

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    APP_ID = os.environ.get("OP_MS_TEAMS_PYTHON_BOT_APP_ID", "")
+    APP_PASSWORD = os.environ.get("OP_MS_TEAMS_PYTHON_BOT_APP_PASSWORD", "")
